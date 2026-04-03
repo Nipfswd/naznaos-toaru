c735c7071a8d78e643d84b9f30a64a0f73e7a479
Nipfswd <allaj0412@gmail.com>
2026-04-04 01:24:07 +0200

    [main] save some cycles on the physical hardware

--- kernel/main.c
+++ kernel/main.c
@@ -164,6 +164,7 @@ int main(struct multiboot *mboot_ptr, uint32_t mboot_mag, uintptr_t esp)
 			kprintf("[%d:%d:%d]", hours, minutes, seconds);
 			restore_csr();
 			__asm__ __volatile__ ("sti");
+			__asm__ __volatile__ ("hlt");
 		}
 	} else {
 		start_shell();
